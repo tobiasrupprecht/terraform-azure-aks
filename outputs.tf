b056e857e6be41967547c47e50d9bdf76a769316
tobiasrupprecht <trupprecht@hashicorp.com>
2022-01-25 14:29:44 +0100

    changed outputs

--- outputs.tf
+++ outputs.tf
@@ -6,17 +6,21 @@ output "kubernetes_cluster_name" {
   value = azurerm_kubernetes_cluster.default.name
 }
 
-# output "host" {
-#   value = azurerm_kubernetes_cluster.default.kube_config.0.host
-# }
+output "host" {
+  value = azurerm_kubernetes_cluster.default.kube_config.0.host
+}
 
-# output "client_key" {
-#   value = azurerm_kubernetes_cluster.default.kube_config.0.client_key
-# }
+output "client_key" {
+  value = azurerm_kubernetes_cluster.default.kube_config.0.client_key
+}
 
-# output "client_certificate" {
-#   value = azurerm_kubernetes_cluster.default.kube_config.0.client_certificate
-# }
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.default.kube_config.0.client_certificate
+}
+
+output "cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate
+}
 
 # output "kube_config" {
 #   value = azurerm_kubernetes_cluster.default.kube_config_raw
